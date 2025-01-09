24cb66b852e19de3ecfe6ff47870b2d971b7909e
meliona03 <azhr.ajemin@gmail.com>
2025-01-09 11:44:41 +0800

    Libraries

---
+++ code.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+import numpy as np
