61877091dc1a9a190291a52e3692f004d317822c
meliona03 <azhr.ajemin@gmail.com>
2025-01-09 11:46:32 +0800

    Reading the file

--- code.py
+++ code.py
@@ -5,3 +5,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+
+# Load the dataset
+dataframe = pd.read_csv('data.csv')
+print(dataframe.head(5))
+
+df = dataframe.copy()
+df.dropna(inplace=True, axis=1)
+print(df.isnull().sum())
+df.drop('id', axis=1, inplace=True)
